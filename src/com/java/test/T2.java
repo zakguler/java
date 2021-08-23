06aa40e03060a74f4dd32b747b72f4e6d530f074
zakguler <Zgul8999>
2021-08-23 10:32:21 -0600

    test push from STS

--- src/com/java/test/T2.java
+++ src/com/java/test/T2.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import com.java.array.arrays_asList.ArraysStream;
 //test push to github
 // another push from intellij
+// from STS-4
 public class T2 {
 
 	// count the words in a string
